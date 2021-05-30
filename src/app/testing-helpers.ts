fbbf36d75c70fa5220d43d282bb3beaf65e1e7f0
ThisALV <lel.charriere@orange.fr>
2021-05-30 13:55:17 +0200

    Testing method for arrays equality

--- src/app/testing-helpers.ts
+++ src/app/testing-helpers.ts
@@ -10,6 +10,23 @@ export function unexpected(): void {
 
 
 /**
+ * Checks for the 2 given arrays to have the same size and to contain the same elements in the exact same order.
+ *
+ * @param a1 compared with `a2`
+ * @param a2 compared with `a1`
+ */
+export function expectArrayToBeEqual<T>(a1: T[], ...a2: T[]): void {
+  // For 2 arrays to be equal element by element, they must have the same size
+  expect(a1).toHaveSize(a2.length);
+
+  // Then checks element by element, as we're sure a1.length === a2.length
+  for (let i = 0; i < a2.length; i++) {
+    expect(a1[i]).toEqual(a2[i]);
+  }
+}
+
+
+/**
  * Mocks any subject which is using next() method to send a message and next() callback to receive a message (for example, a
  * `WebSocketSubject<string>`)
  */
