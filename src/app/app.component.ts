85edbd57d60965cc49b15591e08420a4c7a7ca7f
ThisALV <lel.charriere@orange.fr>
2021-06-14 14:08:57 +0200

    App component displays lobby room when registered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoginService } from './login.service';
 import { Subscription } from 'rxjs';
 import { RptlProtocolService, RptlState } from 'rpt-webapp-client';
-import { filter } from 'rxjs/operators';
-import { ServersListService } from './servers-list.service';
 
 
 /**
@@ -17,22 +15,39 @@ import { ServersListService } from './servers-list.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit, OnDestroy {
+  /**
+   * `true` if client is registered within server and is now inside a Lobby room, `false` if not connected or unregistered.
+   */
+  insideRoom: boolean;
+
   private stateSub?: Subscription; // Subscription for RPTL getState() observer
 
   constructor(
     private readonly rptlProtocol: RptlProtocolService,
-    private readonly loginData: LoginService,
-    private readonly checkoutProcess: ServersListService
-  ) {}
+    private readonly loginData: LoginService
+  ) {
+    this.insideRoom = false;
+  }
 
   /**
-   * Listens for current RPTL state and registers client when it connects to a game server *for other reason than checkout*.
+   * Listens for current RPTL state and registers client when it connects to a game server *for other reason than checkout*, then
+   * displays lobby room when registration is done.
    */
   ngOnInit(): void {
-    this.stateSub = this.rptlProtocol.getState().pipe(filter((s: RptlState) => s === RptlState.UNREGISTERED)).subscribe({
-      next: () => {
-        if (!this.checkoutProcess.isUpdating()) { // Doesn't try to register if we're just checking out the server
-          this.rptlProtocol.register(this.loginData.generateUid(), this.loginData.name); // Uses logins data to register
+    this.stateSub = this.rptlProtocol.getState().subscribe({
+      next: (state: RptlState) => {
+        switch (state) {
+          case RptlState.UNREGISTERED: // When connected, begins registration
+            // CHECKOUT connection will not be caught as their running on CheckoutRptlProtocolService
+            this.rptlProtocol.register(this.loginData.generateUid(), this.loginData.name); // Uses logins data to register
+
+            break;
+          case RptlState.REGISTERED: // Registered inside server, can show room with other actors
+            this.insideRoom = true;
+            break;
+          case RptlState.DISCONNECTED: // No longer inside server, dismisses lobby room
+            this.insideRoom = false;
+            break;
         }
       }
     });
